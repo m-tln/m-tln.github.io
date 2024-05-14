53d15c5c76e8dcf3d9639b987080083dc9f025b4
tul.matvey <tul.matvey@gmail.com>
2024-05-14 16:39:11 +0300

    Local storage

--- scripts/popup-script.js
+++ scripts/popup-script.js
@@ -1,9 +1,14 @@
+localStorage.setItem('flag', 'true')
+
 setTimeout(function() {
-    document.getElementById("popup").style.display = "block";
+    if (localStorage.getItem('flag') === 'true') {
+        document.getElementById("popup").style.display = "block";
+    }
 }, 30000);
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".close-popup").addEventListener("click", function() {
         document.getElementById("popup").style.display = "none";
+        localStorage.setItem('flag', 'false')
     });
 });
